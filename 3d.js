580618f6e4a9fd53dfe706e8677f0017f8009c0f
Patrick Kuo <patrick5267@gmail.com>
2015-03-08 13:29:14 +0800

    add basic canvas view

---
+++ 3d.js
@@ -0,0 +1,45 @@
+function object(vertices, faces) {
+
+  this.vertices = vertices;
+  this.faces = faces;
+
+  this.disp = function (scale) {
+    var canvas = document.getElementById('canvas');
+    var w = canvas.width;
+    var h = canvas.height;
+
+    if (canvas.getContext) {
+      var ctx = canvas.getContext('2d');
+
+      ctx.translate(w/2, h/2);
+
+
+      for(var i = 0; i < faces.length; i++) {
+
+        ctx.beginPath();
+
+
+        for(var j = 0; j < faces[i].length; j++) {
+          var vertex = vertices[parseInt(faces[i][j]) - 1];
+          var next;
+
+          if(j == faces[i].length-1)
+            next = vertices[parseInt(faces[i][0]) - 1];
+          else
+            next = vertices[parseInt(faces[i][j+1]) - 1];
+
+          ctx.moveTo(vertex[0] * scale, vertex[1] * scale);
+          ctx.lineTo(next[0] * scale, next[1] * scale);
+
+        }
+
+        ctx.closePath();
+        ctx.stroke();
+      }
+
+    }
+
+  }
+
+
+}
