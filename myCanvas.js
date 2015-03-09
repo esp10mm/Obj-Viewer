51d9cc62f5a27cdb64ddfa6a75698fed354413a5
Patrick Kuo <patrick5267@gmail.com>
2015-03-09 14:49:22 +0800

    add dotted line implement to canvas.

--- myCanvas.js
+++ myCanvas.js
@@ -9,7 +9,8 @@ function myCanvas(id) {
 
   function mouseDown(e) {
     drag = true;
-    console.log('mouseDown');
+    mouseX = e.pageX;
+    mouseY = e.pageY;
   }
 
   function mouseUp() {
@@ -29,11 +30,40 @@ function myCanvas(id) {
 
   function dragListener(xd, yd) {};
 
+  function dottedLineInit(){
+    var CP = window.CanvasRenderingContext2D && CanvasRenderingContext2D.prototype;
+    if (CP && CP.lineTo){
+      CP.dashedLine = function(x,y,x2,y2,dashArray){
+        if (!dashArray) dashArray=[10,5];
+        if (dashLength==0) dashLength = 0.001; // Hack for Safari
+        var dashCount = dashArray.length;
+        this.moveTo(x, y);
+        var dx = (x2-x), dy = (y2-y);
+        var slope = dx ? dy/dx : 1e15;
+        var distRemaining = Math.sqrt( dx*dx + dy*dy );
+        var dashIndex=0, draw=true;
+        while (distRemaining>=0.1){
+          var dashLength = dashArray[dashIndex++%dashCount];
+          if (dashLength > distRemaining) dashLength = distRemaining;
+          var xStep = Math.sqrt( dashLength*dashLength / (1 + slope*slope) );
+          if (dx<0) xStep = -xStep;
+          x += xStep
+          y += slope*xStep;
+          this[draw ? 'lineTo' : 'moveTo'](x,y);
+          distRemaining -= dashLength;
+          draw = !draw;
+        }
+      }
+    }
+  }
+
   this.init = function() {
     canvas.addEventListener('mousedown', mouseDown, false);
     canvas.addEventListener('mouseup', mouseUp, false);
     canvas.addEventListener('mousemove', mouseMove, false);
     canvas.addEventListener('mouseout', mouseUp, false);
+
+    dottedLineInit();
   }
 
   this.setDragListener = function(func) {
