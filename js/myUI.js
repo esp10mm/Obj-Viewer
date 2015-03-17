28694cdbc3cf29c889977971c3c2ce69a0fe9bec
Patrick Kuo <patrick5267@gmail.com>
2015-03-18 00:41:59 +0800

    add basic ui

---
+++ js/myUI.js
@@ -0,0 +1,27 @@
+$('.panelTitle.segment').on('mousedown', function(e) {
+
+  $(this).parent().addClass('draggable').parents().on('mousemove', function(e) {
+    $('.draggable').offset({
+      top: e.pageY - $('.draggable').outerHeight() / 2,
+      left: e.pageX - $('.draggable').outerWidth() / 2
+    }).on('mouseup', function() {
+      $(this).removeClass('draggable');
+    });
+  });
+  e.preventDefault();
+}).on('mouseup', function() {
+    $('.draggable').removeClass('draggable');
+});
+
+$('.panel.remove.icon').on('click', function(e){
+  $(this).parent().hide();
+})
+
+function activeItem(target) {
+  $('.panel.'+target).show()
+    .css('display', 'inline-block');
+}
+
+function choosefile() {
+  document.getElementById('file').click(); 
+}
